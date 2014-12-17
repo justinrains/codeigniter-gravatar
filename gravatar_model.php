5ff7f9651c01a41ab841d315b8ed4710c1660f5e
Justin Rains <justin@justinrains.com>
2014-12-17 07:22:05 -0500

    Testing.

--- gravatar_model.php
+++ gravatar_model.php
@@ -1,24 +1,24 @@
 <?php
 /*
- * Usage: Put this file in the models directory for your CI install.
- * In the construct of your controller add this:
+Usage: Put this file in the models directory for your CI install.
+In the construct of your controller add:
 $this->load->model('Gravatar_model');
 
- * Size is in square pixels.
+Size is in square pixels.
 
- * Default can be:
+Default can be:
 mm: (mystery-man) a simple, cartoon-style silhouetted outline of a person (does not vary by email hash)
 identicon: a geometric pattern based on an email hash
 monsterid: a generated 'monster' with different colors, faces, etc
 wavatar: generated faces with differing features and backgrounds
 retro: awesome generated, 8-bit arcade-style pixelated faces
 
- * $rating can be:
+$rating can be:
 g: suitable for display on all websites with any audience type.
 pg: may contain rude gestures, provocatively dressed individuals, the lesser swear words, or mild violence.
 r: may contain such things as harsh profanity, intense violence, nudity, or hard drug use.
 x: may contain hardcore sexual imagery or extremely disturbing violence.
- */
+*/
 class Gravatar_model extends CI_Model {
   function __construct() {
       // Call the Model constructor
